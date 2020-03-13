9f835ce01a7b57b175a4945b9ccd19d9f54b35c1
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-13 22:10:10 +0300

    Создать Builder

---
+++ Builder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Standardization_of_acceptance_tests_8_semester
+{
+    public class Flower
+    {
+        public ConsoleColor Color { get; private set; }
+        public float Length { get; private set; }
+
+        public Flower(ConsoleColor color, float length)
+        {
+            Color = color;
+            Length = length;
+        }
+
+        private Flower() {}
+
+        public class Builder
+        {
+            private Flower current = new Flower();
+
+            private dynamic seted = new {isColor = false, isLength = false};
+
+            public Builder SetColor(ConsoleColor color)
+            {
+                current.Color = color;
+                seted.isColor = true;
+                return this;
+            }
+
+            public Builder SetLength(int length)
+            {
+                current.Length = length;
+                seted.isLength = true;
+                return this;
+            }
+
+            public Flower Build()
+            {
+                if(seted.isColor && seted.isLength)
+                    return current;
+                else
+                    throw new Exception(seted);
+            }
+        }
+    }
+}
