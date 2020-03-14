4082a817840d86eed939607d8cac20938993585d
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-14 10:13:14 +0300

    Написать фасад-лист

--- Patterns/Facade.cs
+++ Patterns/Facade.cs
@@ -16,10 +16,86 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Patterns
 {
-    public class Facade
+    public interface IBasicList<T>
     {
-        
+        T this[int index] { get; set; }
+
+        int Count { get; }
+
+        void Add(T item);
+
+        void Insert(int index, T item);
+
+        void RemoveAt(int index);
+    }
+
+    public class Facade<T> : IBasicList<T>, IList<T>
+    {
+        private IList<T> target;
+
+        public T this[int index] { get => target[index]; set => target[index] = value; }
+
+        public int Count => target.Count;
+
+        public void Add(T item)
+        {
+            target.Add(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            target.Insert(index, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            target.RemoveAt(index);
+        }
+
+        #region Скрытые за интерфейс
+
+        bool ICollection<T>.IsReadOnly => target.IsReadOnly;
+
+        void ICollection<T>.Clear()
+        {
+            target.Clear();
+        }
+
+        bool ICollection<T>.Contains(T item)
+        {
+            return target.Contains(item);
+        }
+
+        void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+        {
+            target.CopyTo(array, arrayIndex);
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return target.GetEnumerator();
+        }
+
+        int IList<T>.IndexOf(T item)
+        {
+            return target.IndexOf(item);
+        }
+
+        bool ICollection<T>.Remove(T item)
+        {
+            return target.Remove(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return target.GetEnumerator();
+        }
+
+        #endregion Скрытые за интерфейс
     }
 }
